d0fd4e067077dbf10d4dc63c9efe8c6042952c3b
sgawne2 <sgawne2@gmail.com>
2017-04-07 02:03:10 -0700

    emitting chat messages

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
+
+    socket.on('chat message', function(msg){
+        console.log('chat message: ' + msg);
+    });
+
 });
 
 http.listen(3000, function(){
